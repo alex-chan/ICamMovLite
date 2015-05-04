dd6cfb97ee8bc23edba96f202f58d295087283a5
alex-chan <czzsunset@gmail.com>
2015-05-04 16:35:24 +0800

    Initial Commit

---
+++ ICamMoveLite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ICamMoveLite
+//
+//  Created by Alex Chan on 15/5/4.
+//  Copyright (c) 2015年 Alex Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
